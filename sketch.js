0b7d4456e50fa4d792bc50361a5887305b97e089
shan10832638 <136928126+shan10832638@users.noreply.github.com>
2023-06-18 04:56:44 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://shan10832638.github.io/bird666/model.json';
 
 
 function preload() {
